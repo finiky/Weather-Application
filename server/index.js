4e1597986e32086a8edf3dc4e0ebf98b47529992
finiky <akn2612@gmail.com>
2023-07-16 03:26:14 +0530

    added error checing in index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const app = require("./app");
-const PORT =  process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log("Listening at 5000");
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, (error) => {
+  if (error) {
+    console.log(error);
+  }
+  console.log("Listening at 5000");
+});
