b5b33f63017e0c5820aa6524727f73746aeaf208
finiky <akn2612@gmail.com>
2023-07-16 14:37:42 +0530

    api to get weather completed

--- server/router.js
+++ server/router.js
@@ -3,13 +3,16 @@ const router = express.Router();
 
 router.get("/getweather/:zipid/:countryid", async (request, response, next) => {
   try {
-    const baseUrl = "https://api.openweathermap.org/geo/1.0/zip?zip=";
     const { zipid, countryid } = request.params;
-    const apiId = "&appid=2247808d1c169cf4f00e6f20b7cbcad8";
-    const apiUrl = baseUrl + zipid + "," + countryid + apiId;
-    let data = await fetch(apiUrl);
+    let data = await fetch(
+      `https://api.openweathermap.org/geo/1.0/zip?zip=${zipid},${countryid}&appid=2247808d1c169cf4f00e6f20b7cbcad8`
+    );
     data = await data.json();
-    response.status(200).json(data);
+    let weather = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${data.lat}&lon=${data.lon}&appid=2247808d1c169cf4f00e6f20b7cbcad8`
+    );
+    weather = await weather.json();
+    response.status(200).json({ weather });
   } catch (error) {
     next(error);
   }
