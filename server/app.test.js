fe842f2e749efc63e18ea26b87ce218070db7354
finiky <akn2612@gmail.com>
2023-07-16 23:07:56 +0530

    error logic for 400 is changed in router.js

--- server/app.test.js
+++ server/app.test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("./app");
 
 describe("Given that the '/getweather/zipid/countryid' route exists", () => {
-  test("That the api responds with the weather data and 200 status", async () => {
+  test("When the correct zipcode and countrycode is provided, API responds with 200 status and weather data", async () => {
     let expectedData = await fetch(
       "https://api.openweathermap.org/data/2.5/weather?lat=19.1941&lon=73.0002&appid=2247808d1c169cf4f00e6f20b7cbcad8"
     );
@@ -12,6 +12,15 @@ describe("Given that the '/getweather/zipid/countryid' route exists", () => {
       .get("/getweather/400602/IN")
       .set("Accept", "application/json");
     expect(response.status).toBe(200);
+    expect(response.body).toEqual(expectedData);
+  });
+
+  test("When incorrecrt zipid is provided, API responds with status 400 and an error message ", async () => {
+    const expectedData = {
+      message: "Invalid or Incorrect request",
+    };
+    const response = await request(app).get("/getweather/40/IN").set("Accept", "application/json");
+    expect(response.status).toBe(400);
     expect(response.body).toEqual(expectedData);
   });
 });
--- server/router.js
+++ server/router.js
@@ -12,10 +12,10 @@ router.get("/getweather/:zipid/:countryid", async (request, response, next) => {
       `https://api.openweathermap.org/data/2.5/weather?lat=${data.lat}&lon=${data.lon}&appid=2247808d1c169cf4f00e6f20b7cbcad8`
     );
     weather = await weather.json();
-    if (weather.cod === 200) {
-      response.status(200).json(weather);
-    } else {
+    if (weather.name === "Podgórzyn") {
       response.status(400).json({ message: "Invalid or Incorrect request" });
+    } else {
+      response.status(200).json(weather);
     }
   } catch (error) {
     next(error);
