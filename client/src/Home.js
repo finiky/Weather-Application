52d9d919bf5e7043b6df51bf645cb4259e2ae784
finiky <akn2612@gmail.com>
2023-07-17 23:29:57 +0530

    created form for Home page

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,4 +1,35 @@
-import { useState, useEffect } from "react";
-const Home = () => {};
+import { useState } from "react";
+const Home = () => {
+  const [zip, setZip] = useState("");
+  const [country, setCountry] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <form onSubmit={handleSubmit()}>
+      <div>
+        <lablel>Zip Code:</lablel>
+        <input
+          id="zip"
+          type="text"
+          value={zip}
+          placeholder="Zip Code"
+          onChange={(e) => setZip(e)}
+        />
+      </div>
+      <div>
+        <lablel>Country Code:</lablel>
+        <input
+          id="country"
+          type="text"
+          value={country}
+          placeholder="Country Code"
+          onChange={(e) => setCountry(e)}
+        />
+      </div>
+      <button type="submit">Get Weather Information</button>
+    </form>
+  );
+};
 
 export default Home;
