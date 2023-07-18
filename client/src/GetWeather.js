65b6e7e25126c48101750a789a8544d90871c33c
finiky <akn2612@gmail.com>
2023-07-18 23:03:53 +0530

    added isLoading state to GetWeather component

--- client/src/GetWeather.js
+++ client/src/GetWeather.js
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 const GetWeather = () => {
   const { zipid, countryid } = useParams();
   const [weather, setWeather] = useState({});
+  const [isLoading, setIssLoading] = useState(true);
   useEffect(() => {
     const getData = async () => {
       const response = await fetch(
@@ -11,10 +12,28 @@ const GetWeather = () => {
       );
       const data = await response.json();
       setWeather(data);
+      setIssLoading(false);
     };
     getData();
   }, [zipid, countryid]);
-  return <>{console.log(weather)}</>;
+  if (isLoading) {
+    return (
+      <>
+        <p>is loading...</p>
+      </>
+    );
+  }
+  return (
+    <>
+      <p>{weather.weather[0].main}</p>
+      <p>{weather.weather[0].description}</p>
+      <p>{weather.weather[0].main}</p>
+      <p>{weather.main.temp}</p>
+      <p>{weather.main.feels_like}</p>
+      <p>{weather.main.pressure}</p>
+      <p>{weather.main.humidity}</p>
+    </>
+  );
 };
 
 export default GetWeather;
