2f03d8b479f091b981f81afc153b88ae93c0a1ba
finiky <akn2612@gmail.com>
2023-07-18 23:15:50 +0530

    added error loading message if response is not OK

--- client/src/GetWeather.js
+++ client/src/GetWeather.js
@@ -5,33 +5,38 @@ const GetWeather = () => {
   const { zipid, countryid } = useParams();
   const [weather, setWeather] = useState({});
   const [isLoading, setIssLoading] = useState(true);
+  const [error, setError] = useState(false);
   useEffect(() => {
     const getData = async () => {
       const response = await fetch(
         `http://localhost:5000/getweather/${zipid}/${countryid}`
       );
-      const data = await response.json();
-      setWeather(data);
-      setIssLoading(false);
+      if (response.ok) {
+        const data = await response.json();
+        setWeather(data);
+        setIssLoading(false);
+      }
+      if (!response.ok) {
+        setIssLoading(false);
+        setError(true);
+      }
     };
     getData();
   }, [zipid, countryid]);
   if (isLoading) {
-    return (
-      <>
-        <p>is loading...</p>
-      </>
-    );
+    return <p>Content is loading...</p>;
+  }
+  if (error) {
+    return <p>Error loading the content</p>;
   }
   return (
     <>
-      <p>{weather.weather[0].main}</p>
-      <p>{weather.weather[0].description}</p>
-      <p>{weather.weather[0].main}</p>
-      <p>{weather.main.temp}</p>
-      <p>{weather.main.feels_like}</p>
-      <p>{weather.main.pressure}</p>
-      <p>{weather.main.humidity}</p>
+      <h2>{weather.weather[0].main}</h2>
+      <h3>{weather.weather[0].description}</h3>
+      <h4>{weather.main.temp}</h4>
+      <h5>{weather.main.feels_like}</h5>
+      <h6>{weather.main.pressure}</h6>
+      <h7>{weather.main.humidity}</h7>
     </>
   );
 };
