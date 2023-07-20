bed681f36a7ae4403e4274c331f510e44c21df3b
finiky <akn2612@gmail.com>
2023-07-20 23:43:56 +0530

    corrected errors in home.test.js

--- client/src/Home.test.js
+++ client/src/Home.test.js
@@ -2,20 +2,43 @@ import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import Home from "./Home";
 
-test("Home component has returned correct elements in the document", () => {
-  render(
-    <BrowserRouter>
-      <Home />
-    </BrowserRouter>
-  );
-  const inputElement_1 = screen.getByLabelText(/Zip Code/i);
-  expect(inputElement_1).toBeInTheDocument();
-  const headingElement = screen.getByText("Weather Agent");
-  expect(headingElement).toBeInTheDocument();
-  const inputElement_2 = screen.getByLabelText(/Country Code/i);
-  expect(inputElement_2).toBeInTheDocument();
-  const buttonElement = screen.getByRole("button");
-  expect(buttonElement).toBeInTheDocument();
-  const inputElement = screen.getByPlaceholderText("Zip Code");
-  expect(inputElement).toBeInTheDocument();
+describe("Home component has rendered correctly", () => {
+  test("Input Element 'Zip Code' to be in the documwent", () => {
+    render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
+    const inputElement_1 = screen.getByLabelText(/Zip Code/i);
+    expect(inputElement_1).toBeInTheDocument();
+  });
+
+  test("Input Element 'Country Code' to be in the document", () => {
+    render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
+    const inputElement_2 = screen.getByLabelText(/Country Code/i);
+    expect(inputElement_2).toBeInTheDocument();
+  });
+
+  test("Heading Element 'Weather Agent' to be in the document", () => {
+    render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
+    const headingElement = screen.getByText("Weather Agent");
+    expect(headingElement).toBeInTheDocument();
+  });
+  test("Button Element to be in the document", () => {
+    render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
+    const buttonElement = screen.getByRole("button");
+    expect(buttonElement).toBeInTheDocument();
+  });
 });
