b83938a4bd2cd2052623042a3929ad50e4f754ca
Akshay <98738875+finiky@users.noreply.github.com>
2023-08-27 18:10:06 +0530

    installed node-fetch2

--- server/router.cjs
+++ server/router.cjs
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const dotenv = require("dotenv").config();
-import fetch from "node-fetch";
+const fetch = require("node-fetch");
 router.get("/getweather/:zipid/:countryid", async (request, response) => {
   const { zipid, countryid } = request.params;
   let data = await fetch(
